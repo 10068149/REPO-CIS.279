b0a2d2827a8b07f730b64644d0c4e3ba4b084cef
10068149 <nash.anthony.c@gmail.com>
2017-01-22 11:15:18 -0800

    QUIZ EXERCISE 01 CIS.279/main.cpp

---
+++ QUIZ EXERCISE 01 CIS.279/square.cpp
@@ -0,0 +1,9 @@
+//
+//  square.cpp
+//  CIS.279
+//
+//  Created by CIS on 1/22/17.
+//  Copyright © 2017 CIS.279. All rights reserved.
+//
+
+#include "square.hpp"
---
+++ QUIZ EXERCISE 01 CIS.279/square.hpp
@@ -0,0 +1,19 @@
+//
+//  square.hpp
+//  CIS.279
+//
+//  Created by Anthony Nash on 1/22/17.
+//  Copyright © 2017 CIS.279. All rights reserved.
+//
+
+#ifndef square_hpp
+#define square_hpp
+
+#include <stdio.h>
+
+
+
+
+
+
+#endif /* square_hpp */
