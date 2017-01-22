c30fd1a0606dc25c0625718f0f8286db2be83b87
10068149 <nash.anthony.c@gmail.com>
2017-01-22 12:10:58 -0800

    QUIZ EXERCISE 01 CIS.279/( i ) Quiz Exercise - 01 - Multiple File Compilation CIS.279.pdf

--- QUIZ EXERCISE 01 CIS.279/main.cpp
+++ QUIZ EXERCISE 01 CIS.279/main.cpp
@@ -6,6 +6,37 @@
 //  Copyright © 2017 CIS.279. All rights reserved.
 //
 
+// In this exercise you will demonstrate a multiple file program compilation:...
+/*
+ 
+ In this exercise you will demonstrate a multiple file program compilation:
+ 
+ 1) Declare a main.cpp and a square.cpp file...
+ 2) Provide the sqr( int) function declaration in main...
+ 3) Provide the function definition in another file...
+ 4) Pass in the int value comprised of the rightmost two digits of your GID for the int value to be
+    squared...
+ 
+ For example:
+ 
+ Student:  Anne San Mateo
+ 
+ GID:  G00123456
+ 
+ Pass in the value 56 to be squared.
+ 
+ Before getting started on this assignment become familiar with the topics discussed in our week 1 modules.
+ 
+ Objectives:
+ 
+ The following are the primary objectives of this exercise session:
+ 
+ * Get up and running with your C++ compiler.
+ * Perform a multiple file compilation.
+ 
+ **/
+
+
 #include <iostream>
 #include "square.hpp"
 
@@ -24,11 +55,24 @@ int sqr( int );
 
 
 
-
-
-
 int main (){
     
-    //code here...
+    string student_name_data =" ";
+    int student_ID_data;
+    
+    cout
+    << "*INPUT DATA: Enter your name: ";
+    cin >> student_name_data;
+
+    cout
+    << "*INPUT DATA: Enter your student ID (excluding the: \"G\"): ";
+    cin >> student_ID_data;
+
+    cout
+    << endl
+    << student_name_data << " entered student ID: " << student_ID_data;
+    
+    
+    cout << endl << endl << endl << endl;
     
 }
