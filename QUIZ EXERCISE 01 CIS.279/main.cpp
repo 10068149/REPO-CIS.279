8582728c47320ffad8443c89161f87030073274c
10068149 <nash.anthony.c@gmail.com>
2017-01-22 11:17:48 -0800

    * 11:18 AM, January 22, 2017 (Sun)...

--- QUIZ EXERCISE 01 CIS.279/main.cpp
+++ QUIZ EXERCISE 01 CIS.279/main.cpp
@@ -1,15 +1,34 @@
 //
 //  main.cpp
-//  CIS.279
+//  QUIZ EXERCISE 01 CIS.279
 //
-//  Created by CIS on 1/22/17.
+//  Created by Anthony Nash on 1/22/17.
 //  Copyright © 2017 CIS.279. All rights reserved.
 //
 
 #include <iostream>
+#include "square.hpp"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+using namespace std;
+
+
+
+
+
+
+//Provide the sqr( int) function declaration in main...
+int sqr( int );
+
+
+
+
+
+
+
+
+
+int main (){
+    
+    //code here...
+    
 }
