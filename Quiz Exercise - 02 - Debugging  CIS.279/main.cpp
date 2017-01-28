fb617aa0814cf672f4c116300acb153509729a9c
10068149 <nash.anthony.c@gmail.com>
2017-01-28 10:51:18 -0800

    New Project, * 10:51 AM, January 28, 2017 (Sat)

---
+++ Quiz Exercise - 02 - Debugging  CIS.279/main.cpp
@@ -0,0 +1,36 @@
+//
+//  main.cpp
+//  Quiz Exercise - 02 - Debugging  CIS.279
+//
+//  Created by CIS on 1/28/17.
+//  Copyright © 2017 Anthony Nash. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int sqr ( int ); //function decloraetion; Squar root: sqr()...
+
+
+
+int main ()
+{
+    
+    //Range A: negative inputs whose results is too large to fit (error)
+    //Range B: negative inputs whose result is acceptable (normal)
+    //Range C: positive inputs whose result is acceptable (normal)
+    //Range D: positive input whose result is too large to fit (error)
+    
+    //Fucntion call: sqr ( number ) provide test...
+    //Cases for integer input: number...
+    
+    return 0;
+}
+
+
+
+
+int sqr ( int number )
+{
+    return ( number * number );
+}
