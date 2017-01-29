d619b72853fe36b1ec7f802375f6b58a5f692089
10068149 <nash.anthony.c@gmail.com>
2017-01-28 21:45:32 -0800

    * 9:45 PM, January 28, 2017 (Sat)

--- Quiz Exercise - 02 - Debugging  CIS.279/main.cpp
+++ Quiz Exercise - 02 - Debugging  CIS.279/main.cpp
@@ -1,10 +1,54 @@
+
+
+
+
 //
 //  main.cpp
 //  Quiz Exercise - 02 - Debugging  CIS.279
 //
-//  Created by CIS on 1/28/17.
+//  Created by Anthony Nash on 1/28/17.
 //  Copyright © 2017 Anthony Nash. All rights reserved.
 //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #include <iostream> // for INPUT/OUTPUT
 #include <cassert>  // for ASSERT()
@@ -13,7 +57,86 @@
 
 using namespace std;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int sqr ( int ); //function decloraetion; Squar root: sqr()...
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -21,7 +144,8 @@ int main ()
 {
     srand( time(0) );
     
-    int input_number = ( rand() % 1000000 ) + 1;
+//    int input_number = ( rand() % 500000000 ) + 2147483647;
+    int input_number = 2147483648;
     int exspected_output_number = input_number * input_number;
     
     
@@ -45,10 +169,22 @@ int main ()
     int function_call_reslust = sqr( input_number );
     assert( exspected_output_number == function_call_reslust );
     
+    if ( function_call_reslust > 2147483647 )
+    {
+    
     cout
         << "* FUNCTION RETURN: " << function_call_reslust << endl
         << endl
     << endl;
+        
+    } else
+    {
+    cout
+        << "* ERROR! " << function_call_reslust << " is greater"
+        << " than the maximum number allowed for an INT varable ("
+        << " MAX: 2147483647 )!"
+        << endl;
+    }
     
     
     //Fucntion call: sqr ( number ) provide test...
@@ -65,7 +201,49 @@ int main ()
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int sqr ( int number )
 {
     return ( number * number );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
