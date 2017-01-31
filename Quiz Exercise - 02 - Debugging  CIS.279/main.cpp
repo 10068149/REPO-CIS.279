4ca9c2fac5541345946fb1c279725d6e79e987d9
10068149 <nash.anthony.c@gmail.com>
2017-01-31 11:00:08 -0800

    *UPDATED: 11:00 AM, January 31, 2017 (Tue)

--- Quiz Exercise - 02 - Debugging  CIS.279/main.cpp
+++ Quiz Exercise - 02 - Debugging  CIS.279/main.cpp
@@ -1,7 +1,3 @@
-
-
-
-
 //
 //  main.cpp
 //  Quiz Exercise - 02 - Debugging  CIS.279
@@ -25,49 +21,50 @@ using namespace std;
 
 
 
-int sqr ( int ); //function decloraetion; Square root: sqr()...
+int sqr ( int ); //function declaration; Square root: sqr()...
 void printer ( int );//
-
+int MAX_INT_VALUE_SIZE = 2147483647;
 
 
 int main () {
     
     
     srand( time(0) );
-
-    int input;
+    
+    int input =0;
+    
     
     cout
-        << "// This app is designed to test for the following ranges" << endl
-        << "// of INT type variables, "
-        << "respectively, for both positive " << endl
-        << "// and negative inputs values..." << endl << endl
-        << "* NOTE: The maximum allowed value for INT variable types is: 2147483647."
-        << endl << endl << endl << endl;
-
+    << "// This app is designed to test for the following ranges" << endl
+    << "// of INT type variables, "
+    << "respectively, for both positive " << endl
+    << "// and negative inputs values..." << endl << endl
+    << "* NOTE: The maximum allowed value for INT variable types is: 2147483647."
+    << endl << endl << endl << endl;
+    
     
     
     
     //Range A: negative inputs whose results is too large to fit (error)
     cout
-        << "// Range A: negative inputs whose results is too large to fit (error):"
-        << endl;
-        
+    << "// Range A: negative inputs whose results is too large to fit (error):"
+    << endl;
+    
     input = -46341;
     printer( input );
     
     //Range B: negative inputs whose result is acceptable (normal)
     cout
-        << "// Range B: negative inputs whose result is acceptable (normal): "
-        << endl;
+    << "// Range B: negative inputs whose result is acceptable (normal): "
+    << endl;
     
     input = -( ( rand() % 46339) + 1) ;
     printer( input );
     
     //Range C: positive inputs whose result is acceptable (normal)
     cout
-        << "// Range C: positive inputs whose result is acceptable (normal): "
-        << endl;
+    << "// Range C: positive inputs whose result is acceptable (normal): "
+    << endl;
     
     input = ( rand() % 46339 ) + 1;
     printer( input );
@@ -75,12 +72,12 @@ int main () {
     
     //Range D: positive input whose result is too large to fit (error)
     cout
-        << "// Range D: positive input whose result is too large to fit (error): "
-        << endl;
+    << "// Range D: positive input whose result is too large to fit (error): "
+    << endl;
     
     input = 46341;
     printer( input );
-
+    
     
     
     
@@ -103,19 +100,19 @@ int sqr ( int input ) {
 void printer ( int input ) {
     
     cout
-        << endl
-        << "- INPUT: " << input << endl
-        << "- FUNCTION CALL: SQR( " << input << " );" << endl;
+    << endl
+    << "- INPUT: " << input << endl
+    << "- FUNCTION CALL: SQR( " << input << " );" << endl;
     
     int range = sqr( input );
     
     assert( (input*input) == range );
     
-    if ( (input < 2147483647) && (range > 0) ) {
+    if ( (input < MAX_INT_VALUE_SIZE) && (range >= 0) ) {
         
         cout
         << "- FUNCTION RETURN: " << range << endl
-        << "- RESULTS: This input, " << input << ", when sqaured, is in the"
+        << "- RESULTS: This input, " << input << ", when squared, is in the"
         << " expected" << endl
         << "  range of INT type variables."
         << endl << endl << endl;
@@ -136,10 +133,75 @@ void printer ( int input ) {
     }
     
     
-        
+    
     
 }
-    
-    
-    
-    
+
+
+
+/* --------- OUTPUT: START ----------------------------------------------------
+ 
+ // This app is designed to test for the following ranges
+ // of INT type variables, respectively, for both positive
+ // and negative inputs values...
+ 
+ * NOTE: The maximum allowed value for INT variable types is: 2147483647.
+ 
+ 
+ 
+ // Range A: negative inputs whose results is too large to fit (error):
+ 
+ - INPUT: -46341
+ - FUNCTION CALL: SQR( -46341 );
+ * ERROR: -2147479015
+ This input, -46341, when squared, is out of range
+ of the maximum value allowed for an INT type
+ varable!
+ * NOTE: The maximum allowed value for INT variables is: 2147483647.
+ 
+ 
+ 
+ // Range B: negative inputs whose result is acceptable (normal):
+ 
+ - INPUT: -28764
+ - FUNCTION CALL: SQR( -28764 );
+ - FUNCTION RETURN: 827367696
+ - RESULTS: This input, -28764, when sqaured, is in the expsected
+ range of INT type variables.
+ 
+ 
+ // Range C: positive inputs whose result is acceptable (normal):
+ 
+ - INPUT: 3845
+ - FUNCTION CALL: SQR( 3845 );
+ - FUNCTION RETURN: 14784025
+ - RESULTS: This input, 3845, when sqaured, is in the expsected
+ range of INT type variables.
+ 
+ 
+ // Range D: positive input whose result is too large to fit (error):
+ 
+ - INPUT: 46341
+ - FUNCTION CALL: SQR( 46341 );
+ * ERROR: -2147479015
+ This input, 46341, when squared, is out of range
+ of the maximum value allowed for an INT type
+ varable!
+ * NOTE: The maximum allowed value for INT variables is: 2147483647.
+ 
+ 
+ 
+ 
+ 
+ Program ended with exit code: 0
+ 
+ --------- OUTPUT: END ------------------------------------------------------*/
+
+
+
+
+
+
+
+
+
