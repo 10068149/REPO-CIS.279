68ce3a5071a1c84161fbc316a55b644269b2b30e
10068149 <nash.anthony.c@gmail.com>
2017-01-31 09:46:43 -0800

    * 9:46 AM, January 31, 2017 (Tue)

--- Quiz Exercise - 02 - Debugging  CIS.279/main.cpp
+++ Quiz Exercise - 02 - Debugging  CIS.279/main.cpp
@@ -101,147 +101,244 @@ using namespace std;
 
 
 
-
-
-int sqr ( int ); //function decloraetion; Squar root: sqr()...
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-int main ()
-{
-    srand( time(0) );
-    
-//    int input = ( rand() % 2147483647 ) + 2147483647;
-    int input = 5;
-//    int exspected_output = input * input;
-//    int exspected_output = 25;
     
     
-    //Fucntion call: sqr ( number ) provide test...
-    //Cases for integer input: number...
-
     
     
-    //Range A: negative inputs whose results is too large to fit (error)
     
-    //Range B: negative inputs whose result is acceptable (normal)
     
-    //Range C: positive inputs whose result is acceptable (normal)
     
-    //Range D: positive input whose result is too large to fit (error)
-    cout
-        << "//Range D: positive input whose result is too large to fit (error)..."
-        << endl << endl;
     
-    cout
-        << "* INPUT: " << input << endl;
     
-//    cout
-//        << "* EXSPECTED OUTPUT: " << exspected_output << endl;
-
-    cout
-        << "* FUNCTION CALL: SQR( " << input << " );" << endl;
-
     
-    int function_call_reslust = sqr( input );
-//    assert( exspected_output == function_call_reslust );
     
-    if ( function_call_reslust < 2147483647 )
-    {
     
-    cout
-        << "* FUNCTION RETURN: " << function_call_reslust << endl
-        << endl
-    << endl;
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    int sqr ( int ); //function decloraetion; Squar root: sqr()...
+    void printer ( int );//
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    int main () {
         
-    } else
-    {
-    cout
-        << "* ERROR! " << function_call_reslust << " is greater"
-        << " than the maximum number allowed for an INT varable ("
-        << " MAX: 2147483647 )!"
-        << endl;
+        
+        srand( time(0) );
+        
+        //    int input = ( rand() % 2147483647 );
+        //    int input = 2147483647;
+        //    int exspected_output = input * input;
+        //    int exspected_output = 25;
+        
+
+        //INT, Max/Min Value: 2147483647
+        //Square Root of Max int is: 46,340
+        int input;
+        
+        cout
+            << "// This app is designed to test for the following ranges" << endl
+            << "// of INT type variables, "
+            << "respectively, for both positive " << endl
+            << "// and negtaive inputs values..." << endl << endl
+            << "* NOTE: The maximum allowed value for INT variables is: 2147483647."
+            << endl << endl << endl << endl;
+        
+        //Range A: negative inputs whose results is too large to fit (error)
+        cout
+            << "// Range A: negative inputs whose results is too large to fit (error):"
+            << endl;
+            
+        input = -46341;
+        printer( input );
+        
+        //Range B: negative inputs whose result is acceptable (normal)
+        cout
+            << "// Range B: negative inputs whose result is acceptable (normal): "
+            << endl;
+        
+        input = -( ( rand() % 46339) + 1) ;
+        printer( input );
+        
+        //Range C: positive inputs whose result is acceptable (normal)
+        cout
+            << "// Range C: positive inputs whose result is acceptable (normal): "
+            << endl;
+        
+        input = ( rand() % 46339 ) + 1;
+        printer( input );
+        
+        
+        //Range D: positive input whose result is too large to fit (error)
+        cout
+            << "// Range D: positive input whose result is too large to fit (error): "
+            << endl;
+        
+        input = 46341;
+        printer( input );
+        
+        cout
+        << endl << endl ;
+        
+        return 0;
+        
     }
     
     
     
-    cout
-        << endl << endl << endl << endl
-        << endl << endl << endl << endl
-        << endl;
     
-    return 0;
-}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    int sqr ( int input )
+    {
+        return ( input * input );
+    }
+    
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-int sqr ( int number )
-{
-    return ( number * number );
-}
-
-
-
-
-
-
+    void printer ( int input )
+    {
+        
+        cout
+            << endl
+            << "- INPUT: " << input << endl
+            << "- FUNCTION CALL: SQR( " << input << " );" << endl;
+        
+        int range = sqr( input );//givne range value ....
+        
+        assert( (input*input) == range );
+        
+        if ( (input < 2147483647) && (range > 0) ) {
+            
+            cout
+            << "- FUNCTION RETURN: " << range << endl
+            << "- RESULTS: This input, " << input << ", is in the"
+            << " expsected range of INT type variables."
+            << endl << endl << endl;
+            
+        } else
+        {
+            cout
+            
+            << "* ERROR!!! " << range << endl << endl
+            << "* This input, " << input << ", when squared is out of range " << endl
+            << "* of the maximum value allowed for an INT type " << endl
+            << "* varable!"
+            
+            << endl << endl
+            << "* NOTE: The maximum allowed value for INT variables is: 2147483647."
+            
+            << endl << endl << endl << endl ;
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 
 
