793407015e363f391a5310f8dfa13902e1fab80c
10068149 <nash.anthony.c@gmail.com>
2017-01-31 21:52:42 -0800

    *UPDATED: 11:53 AM, January 31, 2017 (Tue)

---
+++ Assignment - 01 - Abstract Data Types and Classes CIS.279/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Assignment - 01 - Abstract Data Types and Classes CIS.279
+//
+//  Created by CIS on 1/31/17.
+//  Copyright © 2017 Anthony Nash. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
