382df0f343193a584e50c67d70218ac8bb2c2cd9
10068149 <nash.anthony.c@gmail.com>
2017-02-27 16:47:49 -0800

    *  4:47 PM,  February 27, 2017 (Mon)

--- Assignment - 02.2 - Container Classes CIS.279/bag.hpp
+++ Assignment - 02.2 - Container Classes CIS.279/bag.hpp
@@ -1,18 +1,95 @@
-//
+
 //  bag.hpp
 //  CIS.279
 //
-//  Created by CIS on 2/27/17.
+//  Created by ANTHONY NASH on 2/26/17.
 //  Copyright © 2017 Anthony Nash. All rights reserved.
-//
+
+//FILE: bag1.h
+//CLASS PROVIDED: bag (part of the namespace main_savitch_3)
+
+// TYPEDEFS and MEMBER CONSTANTS for the bag class:
+// typedef ____ value_type
+// bag::value_type is the data type of the items in the bag.
+// It may be any of the C++ built-in types (int, char, etc.),
+// or a class with a default constructor, an assignment operator,
+// and operators to test for equality (x == y) and non-equality (x != y).
+
+
+
+
+// < "Documentation for a Header File" >
+
+
 
 #ifndef bag_hpp
 #define bag_hpp
 
-//#include <stdio.h>
+#include <stdio.h>
 
+namespace ANTHONY_NASH_BAG {
+    //Container: BAG { ...
+    class bag
+    //a Bag container class that holds a collection of bag_type data
+    //items supporting the operations as specified above in the functional
+    // requirements:
+    
+    {
+        
+    public:
+        
+        typedef int value_type;//as a synonym for a data type: int (Chp. 3, Pg.5)
+        typedef size_t size_type;//"can hold only non-negative numbers" (Chp. 3, Pg.5)
+        static const size_type CAPACITY = 20;
+        
+        // PROTOTYPES for functions used by this demonstration program:
+        
+        //Constructors: bag();...
+        //Constructors: bag();...
+        bag() { used_data =0; }// set the member: used_data to zero
+        
+        
+        //Inserts an item in the bag: VOID()
+        void bag_insert( const value_type& item );
+        //Pre: item + item.bag_count() <= CAPACITY
+        //Pro:
+        
+        //Determines if an item is in the bag: BOOL()
+        bool bag_find();
+        
+        //Determines the number of copies of an item in the bag: VOID()
+        void bag_find_coppies( const value_type& item);
+        
+        //Determines the total number of items in the bag: VOID()
+        size_type bag_count();
+        
+        //Removes (one) items from the bag: VOID()
+        bool bag_erase( value_type& item );
+        // Pre: The target (item) is actually in the bag;
+        // function removes one copy of target and returns true.
+        // Pro: If target is not in the bag, attempting to erase one
+        // copy has no effect on the bag, and the function returns false.
+        // (Chp.3, Pge6)
+        
+        //Removes all items from the bag: VOID()
+        size_type bag_erase_all();
+        
+    private:
+        
+        value_type bag_data [ CAPACITY];
+        size_type used_data;
+        
+        
+    }; //END OF: CONTAINER BAG
 
+}
 
 
 
 #endif /* bag_hpp */
+
+
+
+
+
+
