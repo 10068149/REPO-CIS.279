501aa057cc79a4ee7321ddb23229b1b1b8795061
10068149 <nash.anthony.c@gmail.com>
2017-03-23 12:01:55 -0700

    UPDATED: 23-03-17, 12:01:53

--- Assignment - 03 - Unsorted Linked List, CIS.279/unsorted.hpp
+++ Assignment - 03 - Unsorted Linked List, CIS.279/unsorted.hpp
@@ -23,7 +23,15 @@ public:
     // F(): Initializes list to empty state...
     // PRE: NONE
     // POST: List is empty...
+
     
+    /*
+     CONST: NO!!
+     
+     + f(): designed to be a constructor...
+     + No need for CONST here...
+     
+     **/
 
     
     bool IsFull() const;
@@ -32,6 +40,15 @@ public:
     // PRE: List has been initialaized...
     // POST: "F()" value is: list is full...
     
+    /*
+     
+     CONST: YES!!
+     + bool returns TRUE or FALSE ...
+     + F(): is designed to check if a list is full or not ...
+     + it should only return TRUE / FALSE and no list data should be
+       changed , so I made it CONST to make sure nothing is changed !
+     
+     **/
     
 
     int GetLength() const;
@@ -40,6 +57,13 @@ public:
     // PRE: List has been initialized ...
     // POST: F() = returns of elements in list
 
+    /*
+     
+     CONST: YES!!
+     + returns a value of: INT...
+     + f(): is designed to only CHECK list data; not change it...
+     
+     **/
     
     
     ItemType GetItem( ItemType Item, bool& found) const;
@@ -53,6 +77,16 @@ public:
     //       returned; otherwise found = false and item is returned
     //       List unchnaged
 
+    /*
+     
+     CONST: YES!!
+     + returns a value of: ItemType...
+     + f(): is designed to return the list element that matches a
+            given key (if present)
+     + f(): is not designed to chaneg the keys or data..
+     
+     
+     **/
     
     
     void PutItem(ItemType item);
@@ -63,21 +97,36 @@ public:
     
     // POST:  Item is in the list
 
+    /*
+     CONST: NO!!
+     + f(): is designed to put DATA on the list
+     + f(): needs access to make changes to DATA....
+     **/
     
     
-    void DeleteItem(ItemType item) const;
+    void DeleteItem(ItemType item);
     
     // F():  Deletes the element whose key matches item’s key...
     // PRE:  List has been initialized...
     // POST: One and only one element in list has a key matching item’s key...
 
+    /*
+     CONST: NO!!!
+     + f(): needs access to: delete DATA from list...
+    
+     **/
     
     
-    void ResetList() const;
+    void ResetList() ;
     // F():  current position for an iteration through the list...
     // PRE:  List has been initialized...
     // POST: Current  position is prior to list..
 
+    /*
+     CONST: NO!!
+     + f(): designed to: RESET or Initialize of an iterator...
+     + f(): needs access to make this change...
+     **/
     
     
     ItemType GetNextItem() const;
@@ -90,6 +139,14 @@ public:
     // POST: Current position is updated to next position;
     //       returns a copy of element at current position...
 
+    /*
+     CONST: YES!!
+     + f(): only needs to read the list data to get the next element..
+     + f(): there is no need to change the next element...
+     **/
+
+    
+    
     
 private:
     
