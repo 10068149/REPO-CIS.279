c49682a1fe8c43484a4f4fb51ae06b5a7db45a58
10068149 <nash.anthony.c@gmail.com>
2017-03-23 10:56:31 -0700

    UPDATED: 23-03-17, 10:56:29

--- Assignment - 03 - Unsorted Linked List, CIS.279/unsorted.hpp
+++ Assignment - 03 - Unsorted Linked List, CIS.279/unsorted.hpp
@@ -19,7 +19,8 @@ public:
     
     unsorted();
     
-    // Initializes list to empty state...
+    // "Make Empty"
+    // F(): Initializes list to empty state...
     // PRE: NONE
     // POST: List is empty...
     
@@ -27,7 +28,7 @@ public:
     
     bool IsFull() const;
     
-    // Determine whether list is full...
+    // F(): Determine whether list is full...
     // PRE: List has been initialaized...
     // POST: "F()" value is: list is full...
     
@@ -35,54 +36,56 @@ public:
 
     int GetLength() const;
     
-    // Determins the number of elements in list...
-    // PRE:
-    // POST:
+    // F(): Determins the number of elements in list...
+    // PRE: List has been initialized ...
+    // POST: F() = returns of elements in list
 
     
     
     ItemType GetItem( ItemType Item, bool& found) const;
     
-    // WHAT:    Get list element whose key matches item’s key (if present)
-    // PRE:     List has been initialized;
-    //          Key member of item is initialized...
-    // POST:    If there is an elemnt someitem whos keymatches item's
-    //          key, then found = true and copy of someItem is
-    //          returned; otherwise found = false and item is returned
-    //          List unchnaged
+    // F():  Get list element whose key matches item’s key (if present)
+    // PRE:  List has been initialized;
+    //       Key member of item is initialized...
+    
+    // POST: If there is an elemnt someitem whos keymatches item's
+    //       key, then found = true and copy of someItem is
+    //       returned; otherwise found = false and item is returned
+    //       List unchnaged
 
     
     
     void PutItem(ItemType item);
     
-    // WHAT:    Puts item into list...
-    // PRE:     1) List has been initialized; 2) List is not full;
-    //          3) ITEM IS NOT IN LIST...
-    // POST:    Item is in the list
+    // F():  Puts item into list...
+    // PRE:  1) List has been initialized; 2) List is not full;
+    //       3) ITEM IS NOT IN LIST...
+    
+    // POST:  Item is in the list
 
     
     
-    void DeleteItem(ItemType item);
+    void DeleteItem(ItemType item) const;
     
-    // WHAT:    Deletes the element whose key matches item’s key...
-    // PRE:     List has been initialized...
-    // POST:    One and only one element in list has a key matching item’s key...
+    // F():  Deletes the element whose key matches item’s key...
+    // PRE:  List has been initialized...
+    // POST: One and only one element in list has a key matching item’s key...
 
     
     
     void ResetList() const;
-    // WHAT:    current position for an iteration through the list...
-    // PRE:     List has been initialized...
-    // POST:    Current  position is prior to list..
+    // F():  current position for an iteration through the list...
+    // PRE:  List has been initialized...
+    // POST: Current  position is prior to list..
 
     
     
     ItemType GetNextItem() const;
 
     // F(): Gets the next element in list...
-    // PRE:  1) List has been initialized..
-    //       2) Current position is defined..
-    //       3) Element at current position is not last in list..
+    // PRE: 1) List has been initialized..
+    //      2) Current position is defined..
+    //      3) Element at current position is not last in list..
     
     // POST: Current position is updated to next position;
     //       returns a copy of element at current position...
