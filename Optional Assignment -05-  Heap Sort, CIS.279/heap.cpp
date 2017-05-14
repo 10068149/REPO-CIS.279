f12169e80f28043e129b130531b5e9caed10359f
10068149 <nash.anthony.c@gmail.com>
2017-05-14 12:59:43 -0700

    UPDATED: 14-05-17, 12:59:42

--- Optional Assignment -05-  Heap Sort, CIS.279/heap.cpp
+++ Optional Assignment -05-  Heap Sort, CIS.279/heap.cpp
@@ -24,7 +24,7 @@ void heapSort ( string* data, int size_of_data )
     << "    // HEAPSORT(): DATA[]: a[], int size_of_data: " << size_of_data
     << endl;
 
-    for ( int counter = 0; counter < 1; counter++)
+    for ( int counter = 0; counter < 2 ; counter++)
     {
         
         cout
@@ -42,8 +42,11 @@ void heapSort ( string* data, int size_of_data )
         cout
         << endl
         << "        // HEAPSORT():: SWAP(???)..." << endl
-        << "        // ..."
-        << endl << endl << endl << endl;
+        << "        // ..." << endl;
+        
+        scribe(data, size_of_data);
+        
+        cout << endl << endl << endl << endl;
 
     }//for ( int counter = 0; counter < 1; counter++) {..}
     
@@ -60,62 +63,13 @@ void heapSort ( string* data, int size_of_data )
 
 
 
-/**
- void rebuild(string* left_data, int size_of_data )
- {
- find LARGEST item and swap with the data[0]
- cout << endl << endl << endl;
- scriber( left_data, size_of_data);
- 
- 
- 
- string right_data[ size_of_data];
- 
- 
- for ( int counter = 0; counter < size_of_data ; counter++) {
- 
- for (int index= counter; index < (size_of_data)-1; index++) {
- 
- right_data[index] = left_data[index+1];
- 
- cout << "   *TEST: " << left_data[counter]
- << " compared to " << right_data[index]
- << endl;
- 
- 
- if ( (left_data[counter].compare( right_data[index]) != 0) )
- cout << "*RESULTS: " << left_data[counter]
- << " is NOT equal to " << right_data[index]
- << endl << endl;
- 
- 
- if ( (left_data[counter].compare( right_data[index]) == 0) )
- cout << "*RESULTS: " << left_data[counter]
- << " is EQUAL to " << right_data[index]
- << endl << endl;
- 
- if ( (left_data[counter].compare( right_data[index]) < 0) )
- cout << "*RESULTS: " << left_data[counter]
- << " is LESS than " << right_data[index]
- << endl << endl;
- 
- if ( (left_data[counter].compare( right_data[index]) > 0) )
- cout << "*RESULTS: " << left_data[counter]
- << " is GREATER than " << right_data[index]
- << endl << endl;
- 
- }//FOR()....
- }
- 
- }//rebuild()...
- 
- **///void rebuild( int, string* left_data, int size_of_data )...
+//void rebuild( int, string* left_data, int size_of_data )...
 void rebuild( int index, string* data, int size_of_data)
 {
     
     // find max value using: std::max_element ...
     
-        string largest_value = *max_element( data, data + size_of_data );
+        string largest_value = *max_element( (data+index), data + size_of_data );
         string temporary[0];
 
         cout
@@ -126,35 +80,32 @@ void rebuild( int index, string* data, int size_of_data)
         << endl;
 
     
-    
     // find position using: std::distance...
     
         cout
         << "            // find position using: ..."
         << endl << endl;
 
-        auto position = find( data + 0, data + size_of_data, largest_value);
-        int located = position - (data + 0);
+        auto position = find( data + index, data + size_of_data, largest_value);
+        int located = position - (data + index);
     
         cout
         << "            * LARGEST VALUE: " << *position
         << "; LOCATED AT INDEX: " << located << endl
         << endl << endl;
-    
 
     
-    
     // swap 0 element with: position...
-        temporary [ 0 ] = data[ 0 ]; // MAKING COPY OF MAX VALUE / LOCAITON ...
-        data[ 0 ] = data[ located ]; // ORIGINAL LOCATION SWAPED WITH FOUND MAX-VALUE / LOCAITON ...
+        temporary [ 0 ] = data[ index ]; // MAKING COPY OF MAX VALUE / LOCAITON ...
+        data[ index ] = data[ located ]; // ORIGINAL LOCATION SWAPED WITH FOUND MAX-VALUE / LOCAITON ...
         data[ located] = temporary[ 0 ];// MOVE ORIGINAL TO THE FOUND LOCATION ...
     
         cout
         << "            // swap 0 element with: position..."
         << endl << endl
-        << "            *  data[ 0 ]: " << temporary[0]
-        << "; SWAPED with data [ located: " << located << " ]: " << temporary [ 0 ] << endl
-        << "            *  data [ 0 ] is NOW: " << data [ 0 ]
+        << "            *  DATA [ 0 ]: " << temporary[ 0 ]
+        << "; SWAPED with DATA [ located: " << located << " ]: " << data [ 0 ] << endl
+        << "            *  DATA [ 0 ] is NOW: " << data [ index ]
         << endl << endl;
     
 }
